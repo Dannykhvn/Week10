395bce85cd2bda94a5f4172558f89751b38455c1
danielkhavin <115034985+Dannykhvn@users.noreply.github.com>
2022-11-08 11:09:22 -0800

    Practiced student activity

--- script/script.js
+++ script/script.js
@@ -26,15 +26,43 @@
 // friuts.shift();
 // console.log(friuts);
 
-const person ={
-    name: ["bob","pob"],
-    age: 20,
-    gender: "male",
-    interests: ["music","skiing"],
-}
-console.log(`My name is ${person.name[0]} ${person.name[1]} I am interested in ${person.interests[0]} and ${person.interests[1]}`)
+// const person ={
+//     name: ["bob","pob"],
+//     age: 20,
+//     gender: "male",
+//     interests: ["music","skiing"],
+// }
+// for (let i in person)
+// {
+//     console.log(`My ${i} is ${person[i]}`)
+// }
+// console.log(`My name is ${person.name[0]} ${person.name[1]} I am interested in ${person.interests[0]} and ${person.interests[1]}`)
 
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+    };
+    let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+    };
+    let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+    };
+    let students = [student1, student2, student3];
 
+    for(let i of students)
+    {
+        if (i.GPA > 3.0 && i.program == "CIT")
+        {
+        console.log(i);
+        }
+
+    }
 
 
 
