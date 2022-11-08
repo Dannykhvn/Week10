edbd75073f4a68c0925c543445716a1afbc547b8
danielkhavin <115034985+Dannykhvn@users.noreply.github.com>
2022-11-08 09:54:10 -0800

    loops practice

--- script/script.js
+++ script/script.js
@@ -1,2 +1,11 @@
-alert("I want to go home")
-console.log("I still want to go home")
+// alert("Nothing is working")
+// console.log("I still want to go home")
+
+// for (let i = 2; i <= 1024; i = i * 2  ) {
+//     console.log(i);
+// }
+let i = 0;
+while (i < 1024){
+    i = i * 2;
+    console.log(i);
+}
