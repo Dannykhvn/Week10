f941ea93c08d91a522701b85a7a13a1cc20c4a29
danielkhavin <115034985+Dannykhvn@users.noreply.github.com>
2022-11-08 10:33:49 -0800

    practiced while loops

--- script/script.js
+++ script/script.js
@@ -4,8 +4,17 @@
 // for (let i = 2; i <= 1024; i = i * 2  ) {
 //     console.log(i);
 // }
-let i = 0;
-while (i < 1024){
-    i = i * 2;
-    console.log(i);
+// let i = 1;
+// while (i < 1024){
+//     i = i * 2;
+//     console.log(i);
+// }
+let input = "";
+
+do{
+    input = prompt("What is your name?");
 }
+while(Number(input) || input == 0);
+
+alert("Welcome " + input);
+alert(`Welcome ${input}`);
