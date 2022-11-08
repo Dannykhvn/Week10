53a2397d1f5a839a132576e0da878e757af1534d
danielkhavin <115034985+Dannykhvn@users.noreply.github.com>
2022-11-08 10:55:04 -0800

    Practiced objects

--- script/script.js
+++ script/script.js
@@ -19,13 +19,20 @@
 // alert("Welcome " + input);
 // alert(`Welcome ${input}`);
 
-let friuts = ["apple","banana"];
-friuts.push("orange");
-friuts.pop();
-friuts.unshift("mangos");
-friuts.shift();
-console.log(friuts);
+// let friuts = ["apple","banana"];
+// friuts.push("orange");
+// friuts.pop();
+// friuts.unshift("mangos");
+// friuts.shift();
+// console.log(friuts);
 
+const person ={
+    name: ["bob","pob"],
+    age: 20,
+    gender: "male",
+    interests: ["music","skiing"],
+}
+console.log(`My name is ${person.name[0]} ${person.name[1]} I am interested in ${person.interests[0]} and ${person.interests[1]}`)
 
 
 
